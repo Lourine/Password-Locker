1a45289ed83b59d07c6b90d0fa076ffc465bfb78
Lourine <lourine.millicent@gmail.com>
2020-09-05 02:18:40 +0300

    Add method that saves user to the User_List

--- user.py
+++ user.py
@@ -2,6 +2,14 @@ class User:
   """
   User class that generates new instances of a User
   """
-def _init_ (self, user_name, password):
-  self.user_name = user_name
-  self.password = password
+
+  user_List = [] #empty User List
+  def __init__(self, user_name, password):
+    self.user_name = user_name
+    self.password = password
+  
+  def save_user(self):
+    """
+    Save User Method saves a new user to the User_List
+    """
+    User.user_List.append(self)
