2141732574f4d7c53ba8d2950d450eb89cbbb8a1
Lourine <lourine.millicent@gmail.com>
2020-09-05 02:16:57 +0300

    Add test to check if new user instance has been saved into the User List

--- user_test.py
+++ user_test.py
@@ -22,6 +22,13 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.user_name,"Milly")
         self.assertEqual(self.new_user.password,"1234")
 
+        
+    def test_save_user(self):
+      """
+      test case to test if a new user instance has been saved into the User list
+      """
+      self.new_user.save_user()
+      self.assertEqual(len(User.user_List),1)
 
 if __name__ == '__main__':
     unittest.main()
