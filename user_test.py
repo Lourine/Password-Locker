ff80deec3a4c1e58f96de862d27d2edff0b09169
Lourine <lourine.millicent@gmail.com>
2020-09-06 02:55:18 +0300

    Add test to check if credential exists

--- user_test.py
+++ user_test.py
@@ -130,7 +130,19 @@ class TestCredentials(unittest.TestCase):
         the_credential = Credentials.find_credential("Twitter")
 
         self.assertEqual(the_credential.account,test_credential.account)
+    
+    def test_credential_exist(self):
+        """
+        test to check if we can return a true or false based on whether we find or can't find the credential.
+        """
+        self.new_credential.save_credentials()
+        the_credential = Credentials("Twitter","Macharia","2017kare")  
+        the_credential.save_credentials()
+        credential_is_found = Credentials.if_credential_exist("Twitter")
+        self.assertTrue(credential_is_found)
+    
 
+  
 
 if __name__ == '__main__':
     unittest.main()
